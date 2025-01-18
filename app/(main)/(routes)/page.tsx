825a5cf8e9beb4a167ff693a3d8ff163e26ddd65
ElishaBjerkeset <ejbjerkeset@gmail.com>
2025-01-18 16:24:21 -0600

    Added Signin Features

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,8 +1,10 @@
+import { ClerkProvider, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
+
+      
     <div className="flex flex-col">
-      This is a protected route. 
     </div>
     
   );
