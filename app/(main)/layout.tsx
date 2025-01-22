ae9cd5813a1e010f72a180b468f8da7f08531e9c
ElishaBjerkeset <ejbjerkeset@gmail.com>
2025-01-22 02:10:13 -0600

    added teh ability to create servers without disappearing nav bar, but did not change tailwind

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,4 +1,5 @@
 import { NavigationSidebar } from "@/components/navigation/navigation-sidebar";
+import { ModalProvider } from "@/components/providers/modal-provider";
 
 
 const MainLayout = async ({
@@ -9,7 +10,7 @@ const MainLayout = async ({
     return (
         <div className="h-full">
             <div className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
-        <NavigationSidebar />
+                <NavigationSidebar/>
             </div>
             <main className="md:pl-[72px] h-full">
                 {children}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
-import { clerkMiddleware } from "@clerk/nextjs/server";
-import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import { ClerkProvider } from '@clerk/nextjs'
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { cn } from "@/lib/utils";
+import { ModalProvider } from "@/components/providers/modal-provider";
 
 const font = Open_Sans({
   variable: "--font-open-sans",
@@ -27,10 +27,10 @@ export default function RootLayout({ children }: Readonly<{
   return (
     <ClerkProvider afterSignOutUrl="/">
       <html lang="en" suppressHydrationWarning>
-
         <body className={cn(font.className, "bg-white dark:bg-[#313338]")}>
           <main className={`${font.variable} antialiased`}>
               <ThemeProvider attribute="class" defaultTheme="dark" enableSystem storageKey="discord-theme">
+                <ModalProvider/>
                 {children}
               </ThemeProvider>
           </main>
