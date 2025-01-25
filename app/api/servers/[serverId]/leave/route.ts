821407fab816528b773410e5cc82ebe367907401
ElishaBjerkeset <ejbjerkeset@gmail.com>
2025-01-24 18:10:22 -0600

    added leave and delete server functionality

--- app/api/servers/[serverId]/leave/route.ts
+++ app/api/servers/[serverId]/leave/route.ts
@@ -2,31 +2,6 @@ import { currentProfile } from "@/lib/current-profile";
 import db from "@/lib/db";
 import { NextResponse } from "next/server";
 
-export async function DELETE(
-    req: Request,
-    {params}: {params: {serverId: string}}
-) {
-    try {
-        const profile = await currentProfile();
-
-        if(!profile) {
-            return new NextResponse("Unauthorized", {status: 401});
-        }
-        const server = await db.server.delete({
-            where: {
-                id: params.serverId,
-                profileId: profile.id,
-            }
-        });
-
-        return NextResponse.json(server);
-
-    } catch (error) {
-        console.log("[SERVER_ID_DELETE]", error);
-        return new NextResponse("Internal Error", {status: 500});
-    }
-}
-
 export async function PATCH(
     req: Request,
     {params}: {params: {serverId: string}}
@@ -38,20 +13,33 @@ export async function PATCH(
         if(!profile) {
             return new NextResponse("Unauthorized", {status: 401});
         }
+        if(!params.serverId) {
+            return new NextResponse("Server ID Missing", {status: 400});
+        }
         const server = await db.server.update({
             where: {
                 id: params.serverId,
-                profileId: profile.id,
+                profileId: {
+                    not: profile.id
+                },
+                members: {
+                    some: {
+                        profileId: profile.id
+                    }
+                }
             },
             data: {
-                name,
-                imageUrl,
+                members: {
+                    deleteMany: {
+                        profileId: profile.id
+                    }
+                }
             }
         });
 
         return NextResponse.json(server);
 
-    } catch (error) {
+    } catch(error) {
         console.log("[SERVER_ID_PATCH]", error);
         return new NextResponse("Internal Error", {status: 500});
     }
